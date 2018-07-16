1477625066425c4bf1ef0c107bf695a203b0156c
dmshikoff <dmshikoff@gmail.com>
2018-07-16 09:44:24 -0700

    added scroll effect to navbar for how it works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom";
 import { withAuthentication, AuthenticationService } from "../helpers";
 import logo from "../images/cici-thick.png";
 import "../styling/Navbar.css";
+const scrollToElement = require('scroll-to-element');
 
 const Nav = props => {
   const img = <img className="logo-img" src={logo} alt="logo" />;
@@ -11,7 +12,7 @@ const Nav = props => {
     <Navbar brand={img} right>
       {
         props.authState ?
-        <NavItem>Welcome, {props.authState.first_name}</NavItem> :
+        <span className="welcome-nav-name">Welcome, {props.authState.first_name}</span> :
         null
       }
       {
@@ -21,7 +22,11 @@ const Nav = props => {
         >
           My Dashboard
         </NavItem> : 
-        <NavItem>
+        <NavItem
+        onClick={event => {
+          scrollToElement(".how-title-con")
+        }}
+        >
         How It Works
         </NavItem>
       }
